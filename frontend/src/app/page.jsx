f53dde343cee4c183b2df687a337826600f922dd
Neel Patel <neelpatel805804@gmail.com>
2024-04-22 01:49:54 +0530

    chore: design changes

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div id="homepage" className="bg-black h-screen">
+    <div id="homepage" className="bg-zinc-800 h-screen">
 
     </div>
   );
